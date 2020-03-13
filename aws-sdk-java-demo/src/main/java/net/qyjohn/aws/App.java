e8b68355fbdbef7b9eccc2494dea8f2fba7c4584
Arnauv-Kaushik <61408068+Arnauv-Kaushik@users.noreply.github.com>
2020-03-13 11:09:02 +0530

    Update App.java

--- aws-sdk-java-demo/src/main/java/net/qyjohn/aws/App.java
+++ aws-sdk-java-demo/src/main/java/net/qyjohn/aws/App.java
@@ -5,6 +5,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "ASK's Hello World , DEEPAK!" );
+        System.out.println( "ASK's Hello World, change made via Git !!" );
     }
 }
