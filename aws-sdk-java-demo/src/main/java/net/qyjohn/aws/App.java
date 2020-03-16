ec22887ed764350a5a254c87c7fd10cd2ca8ef58
Arnauv-Kaushik <61408068+Arnauv-Kaushik@users.noreply.github.com>
2020-03-16 09:07:27 +0530

    Update App.java

--- aws-sdk-java-demo/src/main/java/net/qyjohn/aws/App.java
+++ aws-sdk-java-demo/src/main/java/net/qyjohn/aws/App.java
@@ -5,6 +5,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "ASK's Hello World !! AWS22" );
+        System.out.println( "ASK's Hello World !! AWS333" );
     }
 }
